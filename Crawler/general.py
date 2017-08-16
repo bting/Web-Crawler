0d19774d07bf58f9cd028c1f761901984fd1c978
Tingting Bao <baoting.tina@gmail.com>
2017-08-16 11:42:05 -0700

    initial commit

---
+++ Crawler/general.py
@@ -0,0 +1,25 @@
+import os
+
+# Each website you crawl is a seperate project (folder)
+def create_project_dir(directory):
+    if not os.path.exists(directory):
+        print('Creating project ' + directory)
+        os.makedirs(directory)
+
+# Create queue and crawled files (if not created)
+def create_data_files(project_name, base_url):
+    queue = project_name + "queue.txt"
+    crawled = project_name + "crawled.txt"
+    if not os.path.isfile(queue):
+        write_file(queue, base_url)
+    if not os.path.isfile(crawled):
+        write_file(crawled, '')
+
+# Create new files
+def write_file(path, data):
+    f = open(path, 'w')
+    f.write(data)
+    f.close
+
+create_project_dir('test')
+create_data_files('test', 'google.com')
