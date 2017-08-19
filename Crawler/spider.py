c609aff7b71ddf3503f8af1b5498d34497e199e9
Tingting Bao <baoting.tina@gmail.com>
2017-08-18 17:01:43 -0700

    add mutiple thread

--- Crawler/spider.py
+++ Crawler/spider.py
@@ -44,13 +44,14 @@ class Spider:
         html_string = ''
         try:
             response = urlopen(page_url)
-            if response.gethead('Content-Type') == 'text/html':
+            if response.getheader('Content-Type') == 'text/html':
                 html_bytes = response.read()
                 html_string = html_bytes.decode('utf-8')
+
             finder = LinkFinder(Spider.base_url, page_url)
             finder.feed(html_string)
-        except:
-            print('Error: can not crawl page')
+        except Exception as ex:
+            print(ex)
             return set()
         return finder.page_links()
 
